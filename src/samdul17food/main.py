6fa0243966e971a7a2f90a4f0ab35718110d8797
WhiteCapella <whitecapella.official@gmail.com>
2024-09-13 11:00:22 +0900

    csv test

--- src/samdul17food/main.py
+++ src/samdul17food/main.py
@@ -13,4 +13,10 @@ def read_root():
 def food(name: str):
     # 시간을 구함
     # 음식 이름과 시간을 csv 로 저장 -> /code/data/food.csv
-    return {"food": name, "time": "2024-09-15 11:12:13"}
+
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open('/code/data/food.csv', mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow([name, current_time])
+
+    return {"food": name, "time": current_time}
